7423ead3cd102fa37c2f8c9ab6bea1aec66eba51
acmendes <alex.cep@gmail.com>
2022-01-29 14:59:22 -0300

    Adicionando o sorteio das peças, assim como adicionando o react-router no projeto

--- indiv/src/App.js
+++ indiv/src/App.js
@@ -2,7 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import { loadGoogleScript } from './lib/GoogleLogin';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 const googleClientId = "576773905852-tkb6qe5nev0b4t3dted10g3qo21fjq92.apps.googleusercontent.com";
+var amountOfPrices;
 
 function App() {
   
@@ -19,6 +29,8 @@ function App() {
     setName(profile.getName());
     setEmail(profile.getEmail());
     setImageUrl(profile.getImageUrl());
+
+    setRamdonPieces();
   };
   
   const onFailure = () => {
@@ -26,6 +38,8 @@ function App() {
   }
   
   const logOut = () => {
+    console.log(localStorage.getItem('@IndiviDUALITY/piecesQntd'));
+
     (async() => {
       await googleAuth.signOut();
       setIsLoggedIn(false);
@@ -70,54 +84,130 @@ function App() {
     
   }, []);
   
+  function setRamdonPieces() {
+    //var piecesQntd = [1, 0, 1, 3, 2, 4]
+    amountOfPrices = generateRandomIntegerInRange(2, 15);
+
+    var numPawn   = generatePrices(amountOfPrices);
+    var numHorse  = generatePrices(numPawn[1]);  
+    var numBishop = generatePrices(numHorse[1]);    
+    var numTower  = generatePrices(numBishop[1]);
+    var numQueen  = generatePrices(numTower[1]);
+
+    var piecesQntd = [1,numQueen[0], numTower[0], numBishop[0], numHorse[0], numPawn[0]]
+
+    localStorage.setItem('@IndiviDUALITY/piecesQntd', piecesQntd);
+
+  }
+
+  function generateRandomIntegerInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
+  function generatePrices(amountOfPrices) {
+    console.log("Init - amountOfPrices: " + amountOfPrices);
+    var numPrices = generateRandomIntegerInRange(0, amountOfPrices);
+
+    console.log("Price:" + numPrices);
+
+     amountOfPrices = amountOfPrices - numPrices;
+    console.log("Final - amountOfPrices: " + amountOfPrices);
+
+    return [numPrices, amountOfPrices];
+  } 
+  
   
   return (
-    <div className="App">
-    <nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
-      <a class="navbar-brand" href="#">IndiviDUALITY Chess</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    <Router>
+      <div className="App">
+      <nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
+        <a class="navbar-brand" href="/">IndiviDUALITY Chess</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Page 1</a>
-          </li>
-          
-        </ul>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/play">Jogar <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Page 1</a>
+            </li>
+            
+          </ul>
 
-        <ul class="nav navbar-nav ml-auto">
-        {isLoggedIn &&
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <img src={imageUrl} class="w-25 rounded-circle" />
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" onClick={logOut} href="#">Logout</a>
-            </div>
-          </li>
+          <ul class="nav navbar-nav ml-auto">
+          {isLoggedIn &&
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img src={imageUrl} class="w-25 rounded-circle" />
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="/myBoard">My board</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" onClick={logOut} href="#">Logout</a>
+              </div>
+            </li>
+            }
+          </ul>
+        </div>
+      </nav>
+
+      <Switch>
+          <Route exact path="/">
+            {isLoggedIn &&
+            <Home />
+            }
+          </Route>
+          <Route path="/myBoard">
+            {isLoggedIn &&
+            <MyBoard />
+            }
+          </Route>
+          <Route path="/play">
+            {isLoggedIn &&
+            <Dashboard />
+            }
+          </Route>
+        </Switch>
+
+        <header className="App-header">
+          {!isLoggedIn &&
+            <div id="google-signin"></div>
           }
-        </ul>
+        </header>
+        
       </div>
-    </nav>
+    </Router>
+  );
+}
 
-      <header className="App-header">
-        {!isLoggedIn &&
-          <div id="google-signin"></div>
-        }
-        
-        
-         
-      </header>
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
+
+function MyBoard() {
+  return (
+    <div>
+      <h2>myBoard</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
 
 export default App;
